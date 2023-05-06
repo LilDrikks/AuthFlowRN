462a06fd1e5cfbaf13afe3a3d26e5e085c2dd3ca
LilDrikks <muaway.suport31@gmail.com>
2023-05-06 05:24:01 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Router from "./src/routes/router";
+
+const App = () => {
+  return (
+      <Router />
+  );
+};
+
+export default App;
---
+++ src/routes/router.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-purple-800">
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function Router() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
